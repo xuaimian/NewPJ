b7c7c5dc1e9bdb5d34deddd25782d7fa1425093d
spring <zw.xushanshan@gmail.com>
2019-06-22 15:53:18 +0800

    Initial Commit

---
+++ NewProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewProject
+//
+//  Created by spring on 2019/6/22.
+//  Copyright © 2019 spring. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewProject
+//
+//  Created by spring on 2019/6/22.
+//  Copyright © 2019 spring. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
